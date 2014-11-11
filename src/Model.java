8f56a6889b29248293f8ea18af43148dce813958
Cobbleopolis <agounimon@yahoo.com>
2014-11-11 18:26:01 -0500

    Added Interleaving

--- src/Model.java
+++ src/Model.java
@@ -1,12 +1,26 @@
+import model.Vertex;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.*;
 import org.lwjgl.util.glu.GLU;
 import shaders.ShaderUtils;
 
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 public class Model {
+
+	final int floatByteSize = 4;
+
+	final int vertexFloatCount = 3;
+
+	final int colorFloatCount = 4;
+
+	final int floatsPerVertex = vertexFloatCount + colorFloatCount;
+
+	final int vertexFloatSizeInBytes = floatsPerVertex * floatByteSize;
+
+
 	public float[] verts = {
 		-0.5f, 0.5f, 0f,
 		-0.5f, -0.5f, 0f,
@@ -26,21 +40,15 @@ public class Model {
 			1f, 1f, 1f, 1f
 	};
 
-	public int vao_vertices_ID = 0;
+	public int vaoId = 0;
 
-	public int vbo_vertices_Id = 0;
+	public int vbo_inter_Id = 0;
 
-	public int vbo_indices_ID = 0;
-
-	public int vbo_colors_ID = 0;
+	public int vbo_indices_Id = 0;
 
 	public int pId = 0;
 
 	public IntBuffer indicesBuffer = BufferUtils.createIntBuffer(indices.length);
-
-	public FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(verts.length);
-
-	public FloatBuffer colorsBuffer = BufferUtils.createFloatBuffer(colors.length);
 
 	public int vsId = ShaderUtils.loadShader("res/shaders/sprites.vert", GL20.GL_VERTEX_SHADER);
 
@@ -48,40 +56,47 @@ public class Model {
 
 	public void create(){
 		System.out.println("Buffers");
-		verticesBuffer.put(verts);
+		// We'll define our quad using 4 vertices of the custom 'Vertex' class
+		Vertex v0 = new Vertex(); v0.setXYZ(-0.5f, 0.5f, 0f); v0.setRGB(1, 0, 0);
+		Vertex v1 = new Vertex(); v1.setXYZ(-0.5f, -0.5f, 0f); v1.setRGB(0, 1, 0);
+		Vertex v2 = new Vertex(); v2.setXYZ(0.5f, -0.5f, 0f); v2.setRGB(0, 0, 1);
+		Vertex v3 = new Vertex(); v3.setXYZ(0.5f, 0.5f, 0f); v3.setRGB(1, 1, 1);
+
+		Vertex[] vertices = new Vertex[] {v0, v1, v2, v3};
+		// Put each 'Vertex' in one FloatBuffer
+		FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length *
+				Vertex.elementCount);
+		for (int i = 0; i < vertices.length; i++) {
+			verticesBuffer.put(vertices[i].getXYZW());
+			verticesBuffer.put(vertices[i].getRGBA());
+		}
+
 		verticesBuffer.flip();
+
 		indicesBuffer.put(indices);
 		indicesBuffer.flip();
-		colorsBuffer.put(colors);
-		colorsBuffer.flip();
 
 		// Create a new Vertex Array Object in memory and select it (bind)
-		vao_vertices_ID = GL30.glGenVertexArrays();
-		GL30.glBindVertexArray(vao_vertices_ID);
+		vaoId = GL30.glGenVertexArrays();
+		GL30.glBindVertexArray(vaoId);
 
-		System.out.println("Verts");
-		// Create a new Vertex Buffer Object in memory and select it (bind) - VERTICES
-		vbo_vertices_Id = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo_vertices_Id);
+		// Create a new Vertex Buffer Object in memory and select it (bind)
+		vbo_inter_Id = GL15.glGenBuffers();
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo_inter_Id);
 		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, verticesBuffer, GL15.GL_STATIC_DRAW);
-		GL20.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-
-		System.out.println("Colors");
-		// Create a new VBO for the indices and select it (bind) - COLORS
-		vbo_colors_ID = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vbo_colors_ID);
-		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, colorsBuffer, GL15.GL_STATIC_DRAW);
-		GL20.glVertexAttribPointer(1, 4, GL11.GL_FLOAT, false, 0, 0);
+		// Put the positions in attribute list 0
+		GL20.glVertexAttribPointer(0, 4, GL11.GL_FLOAT, false, Vertex.sizeInBytes, 0);
+		// Put the colors in attribute list 1
+		GL20.glVertexAttribPointer(1, 4, GL11.GL_FLOAT, false, Vertex.sizeInBytes,
+				Vertex.elementBytes * 4);
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
 
 		// Deselect (bind to 0) the VAO
 		GL30.glBindVertexArray(0);
 
-		System.out.println("Inds");
 		// Create a new VBO for the indices and select it (bind) - INDICES
-		vbo_indices_ID = GL15.glGenBuffers();
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vbo_indices_ID);
+		vbo_indices_Id = GL15.glGenBuffers();
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vbo_indices_Id);
 		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL15.GL_STATIC_DRAW);
 		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
 		setupShaders();
@@ -92,12 +107,12 @@ public class Model {
 		GL20.glUseProgram(pId);
 
 		// Bind to the VAO that has all the information about the vertices
-		GL30.glBindVertexArray(vao_vertices_ID);
+		GL30.glBindVertexArray(vaoId);
 		GL20.glEnableVertexAttribArray(0);
 		GL20.glEnableVertexAttribArray(1);
 
 		// Bind to the index VBO that has all the information about the order of the vertices
-		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vbo_indices_ID);
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vbo_indices_Id);
 
 		// Draw the vertices
 		GL11.glDrawElements(GL11.GL_TRIANGLES, indices.length, GL11.GL_UNSIGNED_INT, 0);
@@ -119,9 +134,25 @@ public class Model {
 		GL20.glDeleteShader(vsId);
 		GL20.glDeleteShader(fsId);
 		GL20.glDeleteProgram(pId);
-		// Delete the color VBO
+
+		// Select the VAO
+		GL30.glBindVertexArray(vaoId);
+
+		// Disable the VBO index from the VAO attributes list
+		GL20.glDisableVertexAttribArray(0);
+		GL20.glDisableVertexAttribArray(1);
+
+		// Delete the vertex VBO
 		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-		GL15.glDeleteBuffers(vbo_colors_ID);
+		GL15.glDeleteBuffers(vbo_inter_Id);
+
+		// Delete the index VBO
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+		GL15.glDeleteBuffers(vbo_indices_Id);
+
+		// Delete the VAO
+		GL30.glBindVertexArray(0);
+		GL30.glDeleteVertexArrays(vaoId);
 	}
 
 	private void setupShaders() {
